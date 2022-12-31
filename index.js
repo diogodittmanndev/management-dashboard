d6e5a516fca48fd7ab97a66a1522b1064531a7b3
diogodittmanndev <diogo.dyaot@gmail.com>
2022-12-31 19:32:12 -0300

    Update Sidebar Config

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ $(document).ready(function(){
         $(this).find('.dropdown').toggleClass('rotate');
     });
 });
+
+// Function for close and open sidebar
+$('.sidebar__menu-btn').click(function(){
+    $('.sidebar').addClass('active');
+    // $('.sidebar__menu-btn').css("visibility", "hidden");
+})
+
+$('.sidebar__close-btn').click(function(){
+    $('.sidebar').removeClass('active');
+    // $('.sidebar__menu-btn').css("visibility", "hidden");
+})
+
+// Function to adjust main content
+$('.sidebar__menu-btn').click(function(){
+    $('.main').addClass('active');
+    // $('.sidebar__menu-btn').css("visibility", "hidden");
+})
+
+$('.sidebar__close-btn').click(function(){
+    $('main').removeClass('active');
+    // $('.sidebar__menu-btn').css("visibility", "hidden");
+})
